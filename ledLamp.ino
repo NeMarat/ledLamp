8eb8d9515f20179520e06728c1079cf3c1e8a8f1
NeMarat <maratn@gmail.com>
2016-02-08 22:49:01 +0300

    .

--- ledLamp.ino
+++ ledLamp.ino
@@ -15,37 +15,26 @@ typedef enum { COL_RED = 0, COL_GRN, COL_BLU } cur_col;
 uint8_t enc_slave = LOW;
 int8_t enc_val = 0;
 
+uint64_t cur_ms;
+uint64_t last_ms;
+
+bool btn_pressed;
+
 void encoder_slave () {
   enc_slave = digitalRead(ENC_PIN_2);
 }
 
-void encoder_main {
+void encoder_main () {
   if (enc_slave == HIGH) { enc_val++; } 
    else { enc_val--; }
 }
 
 void adjust_col (cur_col c_olor, int8_t v_al) {
   switch (c_olor) {
-    case COL_RED: red_pwm += v_al; break; // it shoud go back to 0 if > 255 because type overflow
-    case COL_GRN: grn_pwm += v_al; break;
-    case BLU_GRN: blu_pwm += v_al; break;
+    case COL_RED: red_pwm += v_al; EEPROM.write(RED_MEM, red_pwm); break; // it shoud go back to 0 if > 255 because type overflow
+    case COL_GRN: grn_pwm += v_al; EEPROM.write(GRN_MEM, grn_pwm); break;
+    case BLU_GRN: blu_pwm += v_al; EEPROM.write(BLU_MEM, blu_pwm); break;
   }
-}
-
-void writeULong (unsigned long l, int address) {
-  EEPROM.write(address + 0, *((byte*)&l + 0)); //byte #1 from long
-  EEPROM.write(address + 1, *((byte*)&l + 1)); // byte #2
-  EEPROM.write(address + 2, *((byte*)&l + 2)); // #3
-  EEPROM.write(address + 3, *((byte*)&l + 3)); // #4
-}
-
-unsigned long readULong(int address) {
-  unsigned long l = 0;
-  *((byte*)&l + 0) = EEPROM.read(address + 0);
-  *((byte*)&l + 1) = EEPROM.read(address + 1);
-  *((byte*)&l + 2) = EEPROM.read(address + 2);
-  *((byte*)&l + 3) = EEPROM.read(address + 3);
-  return l;
 }
 
 void setup() {
@@ -63,9 +52,25 @@ void setup() {
   attachInterrupt(0, encoder_main, FALLING);
   attachInterrupt(1, encoder_slave, CHANGE);
 
+  btn_pressed=false;
 }
 
-void loop() 
-{
+void loop() {
+  cur_ms=millis;
+  if (digitalWrite(BTN_PIN) == LOW && btn_pressed==false) {
+    last_ms=millis();
+    btn_pressed=true;
+    if (cur_state == ADJ_NONE) { cur_state=ADJ_BRIGHT; }
+  } 
+  if (digitalWrite(BTN_PIN) == HIGH && btn_pressed==true) {
+    if (cur_ms - last_ms > 2000) { cur_state=ADJ_RED; }
+     else { if (cur_state == ADJ_BRIGHT) { cur_state=ADJ_NONE; }
+            if (cur_state == ADJ_RED) { cur_state=ADJ_GRN; }
+            if (cur_state == ADJ_GRN) { cur_state=ADJ_BLU; }
+            if (cur_state == ADJ_BLU) { cur_state=ADJ_RED; }
+            if (cur_state == ADJ_NONE) { cur_state=ADJ_BRIGHT; }
+     }
+    btn_pressed=false;
+  }
 
 }
